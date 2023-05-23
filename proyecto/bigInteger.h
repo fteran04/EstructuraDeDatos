597b16ee72348fd42a7b0b947135960ae616c3c7
fteran04 <110037380+fteran04@users.noreply.github.com>
2023-05-23 16:15:54 -0500

    Update bigInteger.h

--- proyecto/bigInteger.h
+++ proyecto/bigInteger.h
@@ -7,38 +7,44 @@
 
 using namespace std;
 
-
 class bigInteger{
     private:
+    bool sign;
     vector<int> vec;
 
     public:
 
     /* Contructoras */
-    void stringToBigInteger(string &cad);
-    bigInteger copyBigInteger(bigInteger &bigInt);
+    bigInteger();
+    bigInteger( string&);
+    bigInteger( bigInteger&);
 
     /* Modificadoras */
-    void add(bigInteger &bigInt);
-    void product(bigInteger &bigInt);
-    void substract(bigInteger &bigInt);
-    void quotient(bigInteger &bigInt);
-    void remainder(bigInteger &bigInt);
-    void pow(bigInteger &bigInt);
+    void add(bigInteger &);
+    void product(bigInteger &);
+    void substract(bigInteger &);
+    void quotient(bigInteger &);
+    void remainder(bigInteger &);
+    void pow(int);
 
     /* Analizadoras */
     string toString();
+    int get(int);
+    int size();
+    bool getsign();
 
     /* Sobrecargas de operadores */
-    bigInteger operator+(const bigInteger &bigInt);
-    bigInteger operator-(const bigInteger &bigInt);
-    bigInteger operator*(const bigInteger &bigInt);
-    bigInteger operator/(const bigInteger &bigInt);
-    bigInteger operator%(const bigInteger &bigInt);
-    bigInteger operator==(const bigInteger &bigInt);
-    bigInteger operator<(const bigInteger &bigInt);
-    bigInteger operator<=(const bigInteger &bigInt);
+    
+    //bigInteger operator+(bigInteger &bigInt);
+    //bigInteger operator-(bigInteger &bigInt);
+    //bigInteger operator*(bigInteger &bigInt);
+    //bigInteger operator/(bigInteger &bigInt);
+    //bigInteger operator%(bigInteger &bigInt);
+    bool operator==(bigInteger &);
+    bool operator<(bigInteger &);
+    bool operator<=(bigInteger &);
+    
 
 };
-#endif
 
+#endif
