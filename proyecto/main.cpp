f705f86b79b481b50b50b0b76423f27265f25884
fteran04 <110037380+fteran04@users.noreply.github.com>
2023-05-29 17:47:08 -0500

    Update main.cpp

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -1,116 +1,108 @@
 #include "bigInteger.h"
 #include <iostream>
 
-using namespace std;
-
-
 int main(){
+    int i = 1, j;
+    string a, b;
 
-    cout << "############### Constructor con string ###############" << endl;
-    /*Constructor con string*/
-    string a = "-123", b = "1", c = "99", d = "123";
-    bigInteger b1(a);
-    string bigCad = b1.toString();
-    cout << "bigInteger b1 = " << bigCad << endl;
-  
-    /*Constructor copiando un bigInteger*/
-    bigInteger b2(b1);
-    string bigCad2 = b2.toString();
-    cout << "bigInteger b2 = " << bigCad2 << endl << endl;
+    string aInt = "";
+    string bInt = "";
+    string aFloat = "";
+    string bFloat = "";
 
-    cout << "######################## add ########################" << endl;
-    /*add*/
-    b1.add(b2);
-    bigCad = b1.toString();
-    cout << "b1 + b2 = " << bigCad << endl;
+    BigInteger bigInt1, bigInt2, bigFloat1, bigFloat2;
 
-    /////////////////////////////////////
+    bool flag;
 
-    bigInteger b3(b);
-    bigInteger b4(c);
-    b3.add(b4);
-    bigCad2 = b3.toString();
-    cout << "b3 + b4 = " << bigCad2 << endl << endl;
+    while(cin >> a){
+        cin >> b;
+        j = 0;
+        flag = true;
 
+        /* ################ Lectura del BigInteger A ################ */
+        /*Parte entera*/
+        while(j < a.size() && flag){
+            if(a[j] == '.'){
+            flag = false;
+            }
+            else{
+            aInt = aInt + a[j];
+            }
+            j++;
+        }
+        /*Parte decimal*/
+        while(j < a.size()){
+            aFloat += a[j];
+            j++;
+        }
 
-    /*Sobrecargas de operadores*/
-    cout << "############## Sobrevargas de operadores ##############" << endl;
-    /*     ==      */
-    bigInteger bs1(a);
-    bigInteger bs2(a);
-    bigInteger bs3(b);
-    bigInteger bs4(d);
+        j = 0;
+        flag = true;
 
-    cout << "bs1 = -123" << endl << "bs2 = -123" << endl;
-    if(bs1 == bs2)
-    cout << "bs1 == bs2 = true" << endl;
-    else
-    cout << "bs1 == bs2 = false" << endl;
+        /* ################ Lectura del BigInteger b ################ */
+        /*Parte entera*/
+        while(j < b.size() && flag){
+            if(b[j] == '.')
+            flag = false;
+            else
+            bInt += b[j];
+            j++;
+        }
+        /*Parte decimal*/
+        while(j < b.size()){
 
-    cout << endl;
-    cout << "bs1 = -123" << endl << "bs3 = 1" << endl;
-    if(bs1 == bs3)
-    cout << "bs1 == bs2 = true" << endl;
-    else
-    cout << "bs1 == bs2 = false" << endl;
-    cout << "----------------------------------------------------" << endl;
+            bFloat += b[j];
+            j++;
+        }
 
-    /*     <     */
-    cout << endl;
-    cout << "bs1 = -123" << endl << "bs4 = 123" << endl;
-    if(bs1 < bs4)
-    cout << "bs1 < bs4 = true" << endl;
-    else
-    cout << "bs1 < bs4 = false" << endl;
+        /* Comparar*/
+        bigInt1 = BigInteger(aInt);
+        bigInt2 = BigInteger(bInt);
+        
 
-    cout << endl;
-    cout << "bs1 = -123" << endl << "bs3 = 1" << endl;
-    if(bs1 < bs3)
-    cout << "bs1 < bs3 = true" << endl;
-    else
-    cout << "bs1 < bs3 = false" << endl;
+        if(bigInt1 < bigInt2){
+            cout << "Case " << i << ": Smaller" << endl;
+        }
+        else if(bigInt2 < bigInt1){
+            cout << "Case " << i << ": Bigger" << endl;
+        }
+        else{
+            if(aFloat.size() < bFloat.size()){
+                while(aFloat.size() != bFloat.size()){
+                    aFloat += '0';
+                }
+            }
 
-    cout << endl;
-    cout << "bs1 = -123" << endl << "bs2 = -123" << endl;
-    if(bs1 < bs2)
-    cout << "bs1 < bs2 = true" << endl;
-    else
-    cout << "bs1 < bs2 = false" << endl;
-    cout << "----------------------------------------------------" << endl;
+            else if(bFloat.size() < aFloat.size()){
+                while(aFloat.size() != bFloat.size()){
+                    bFloat += '0';
+                }
+            }
+            //cout << "aFloat = " << aFloat << endl << "bFloat = " << bFloat << endl;
+            aFloat = '1' + aFloat;
+            bFloat = '1' + bFloat;
 
-    /*    <=    */
+            bigFloat1 = BigInteger(aFloat);
+            bigFloat2 = BigInteger(bFloat);
 
-    cout << endl;
-    cout << "bs1 = -123" << endl << "bs4 = 123" << endl;
-    if(bs1 <= bs4)
-    cout << "bs1 <= bs4 = true" << endl;
-    else
-    cout << "bs1 <= bs4 = false" << endl;
+            if(bigFloat1 < bigFloat2){
+            cout << "Case " << i << ": Smaller" << endl;
+            }
+            else if(bigFloat2 < bigFloat1){
+                cout << "Case " << i << ": Bigger" << endl;
+            }
+            else{
+                cout << "Case " << i << ": Same" << endl;
+            }
+        }
 
-    cout << endl;
-    cout << "bs1 = -123" << endl << "bs2 = -123" << endl;
-    if(bs1 <= bs2)
-    cout << "bs1 <= bs2 = true" << endl;
-    else
-    cout << "bs1 <= bs2 = false" << endl << endl;
-
-    cout << "###################### substract ######################" << endl;
-    string sr1 = "80", sr2 = "40";
-    bigInteger br1(sr1);
-    bigInteger br2(sr2);
-
-    cout << "br1 = 80" << endl;
-    cout << "br2 = 40" << endl;
-    br1.substract(br2);
-    bigCad = br1.toString();
-    cout << "br1 - br2 = " << bigCad << endl;
-
-
-
-
+        aInt = "";
+        bInt = "";
+        aFloat = "";
+        bFloat = "";
+        a = "";
+        b = "";
+        i++;
+    }
     return 0;
-
-    
-
-
 }
